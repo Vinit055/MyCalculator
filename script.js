454e11e52c98688094bed451bcbed01dbefaadc8
Vinit Damania <vinitdamania@gmail.com>
2024-06-12 20:31:42 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementsByClassName("buttons")[0].style.backgroundColor="darkorange";
+
+document.getElementsByClassName("buttons")[4].style.backgroundColor="darkorange";
+
+document.getElementsByClassName("buttons")[8].style.backgroundColor="darkorange";
+
+document.getElementsByClassName("buttons")[12].style.backgroundColor="darkorange";
+
+let currentDisplay = '';
+document.querySelector('#display').value = currentDisplay;
